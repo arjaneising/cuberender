c8f52313c190b5bf68bf06eb0bae70c50f1b555e
Arjan Eising <arjan@arjaneising.nl>
2015-08-19 22:34:25 +0200

    Add License information

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> / <%= grunt.template.today("yyyy-mm-dd") %> / License: MIT */\n'
       },
       build: {
         src: 'src/script/cuberender.js',
